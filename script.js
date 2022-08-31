77963a480cfdde885e3a9023e9b0cdaa3053d0c1
JBrendon15 <110281109+JBrendon15@users.noreply.github.com>
2022-08-31 18:12:03 -0500

    added generatePassword function and some of the code

--- script.js
+++ script.js
@@ -12,3 +12,34 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+function generatePassword () {
+  let upperChar = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  let lowerChar = 'abcdefghijklmnopqrstuvwxyz';
+  let numbers = '1234567890'
+  let special = "\ \!\"\#\$\%\&\'\(\)\*\+\,\-\.\/\:\;\<\=\>\?\@\[\\\]\^\_\`\{\|\}\~";
+  let generatedPassword = 'Hello';
+  let wantedChar = '';
+  let includeUpper = confirm("Would you like uppercase characters in your password?");
+  if (includeUpper) {
+    wantedChar = wantedChar + upperChar;
+  }
+  let includeLower = confirm("would you like lowercase characters in your password?");
+  if (includeLower) {
+    wantedChar = wantedChar + lowerChar;
+  }
+  let includeSpecial = confirm("Would you like special characters in your password?");
+  if (includeSpecial) {
+    wantedChar = wantedChar + special;
+  }
+  let includeNum = confirm("Would you like numbers in your password?");
+  if (includeNum){
+    wantedChar = wantedChar + numbers;
+  }
+  if ((!includeUpper) && (!includeLower) && (!includeSpecial) && (!includeNum)) {
+    alert('You need to include at least one type of character!');
+    generatePassword();
+  }
+return generatedPassword;
+}
+
