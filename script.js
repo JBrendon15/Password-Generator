4b427ea3ee9ec1968e99faf8fb39f6466a311f95
JBrendon15 <110281109+JBrendon15@users.noreply.github.com>
2022-08-31 19:09:39 -0500

    finished the generatePassword function and added validations to the input for password length

--- script.js
+++ script.js
@@ -13,33 +13,44 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-function generatePassword () {
+function generatePassword() {
   let upperChar = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
   let lowerChar = 'abcdefghijklmnopqrstuvwxyz';
   let numbers = '1234567890'
-  let special = "\ \!\"\#\$\%\&\'\(\)\*\+\,\-\.\/\:\;\<\=\>\?\@\[\\\]\^\_\`\{\|\}\~";
-  let generatedPassword = 'Hello';
+  let special = "\!\"\#\$\%\&\'\(\)\*\+\,\-\.\/\:\;\<\=\>\?\@\[\\\]\^\_\`\{\|\}\~";
+  let generatedPassword = '';
   let wantedChar = '';
-  let includeUpper = confirm("Would you like uppercase characters in your password?");
+  
+  let includeUpper = confirm(`Would you like uppercase characters in your password?`);
   if (includeUpper) {
     wantedChar = wantedChar + upperChar;
   }
-  let includeLower = confirm("would you like lowercase characters in your password?");
+  let includeLower = confirm(`would you like lowercase characters in your password?`);
   if (includeLower) {
     wantedChar = wantedChar + lowerChar;
   }
-  let includeSpecial = confirm("Would you like special characters in your password?");
+  let includeSpecial = confirm(`Would you like special characters in your password?`);
   if (includeSpecial) {
     wantedChar = wantedChar + special;
   }
-  let includeNum = confirm("Would you like numbers in your password?");
+  let includeNum = confirm(`Would you like numbers in your password?`);
   if (includeNum){
     wantedChar = wantedChar + numbers;
   }
   if ((!includeUpper) && (!includeLower) && (!includeSpecial) && (!includeNum)) {
-    alert('You need to include at least one type of character!');
+    alert(`You need to include at least one type of character!`);
     generatePassword();
   }
+  let passwordLength = prompt(`How many characters would you like to include in your password? (Must be between 8 and 128 characters)`)
+  
+  if ((parseInt(passwordLength) < 8) || (parseInt(passwordLength) > 128) || (!parseInt(passwordLength))) {
+    alert(`Your password needs to be a NUMBER between 8 and 128!`)
+    return '';
+  }
+  for(let i = 0; i < parseInt(passwordLength); i++){
+    let randomChar = wantedChar[Math.floor(Math.random()*wantedChar.length)];generatedPassword = generatedPassword + randomChar;
+  }
+console.log(generatedPassword.length);
 return generatedPassword;
 }
 
